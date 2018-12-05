c66a051c81e21ab45d5c3a204d04f2d8080622f2
Joren Vandeweyer <jorenvandeweyer@gmail.com>
2018-12-05 10:33:01 +0100

    day 5

---
+++ 2018/5_1.js
@@ -0,0 +1,30 @@
+const getInput = require("../importInput");
+
+function isReactive(x, y) {
+    return Math.abs(x.charCodeAt(0)-y.charCodeAt(0)) === 32;
+}
+
+function reaction(s) {
+    let string = s;
+    for (let i = 0; i < string.length-1; i++) {
+        console.log(string[i], string[i+1]);
+        if (isReactive(string[i], string[i+1])) {
+            string = string.substr(0, i) + string.substr(i+2, string.length);
+            i-=2;
+            if (i<0) i=-1;
+        }
+    }
+    return string;
+}
+
+
+async function main() {
+    const input = (await getInput("./input_5.txt")).raw;
+    result = reaction(input);
+    console.log(result, result.length);
+}
+
+
+
+
+main();
---
+++ importInput.js
@@ -0,0 +1,9 @@
+const fs = require("fs").promises;
+
+module.exports = async (path) => {
+    const file = await fs.readFile(path, "utf8");
+    return {
+        raw: file.trim(),
+        lines: file.trim().split("\n"),
+    }
+}
